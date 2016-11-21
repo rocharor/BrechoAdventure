74b4496ecdcb86b0765993be58652f9e304a1280
roliveira <roliveira@ssol.local>
2016-11-21 17:32:44 -0200

    Ajuste email

---
+++ resources/views/email/respAutomatica.blade.php
@@ -0,0 +1,13 @@
+@extends('respAutomatica.blade.php');
+@section('content');
+    <tr>
+        <td style='background-color: #fedd7a; font-family: Arial, Helvetica, sans-serif; font-size: 13px; color: #414042; padding: 20px; line-height: 15px;'>
+            <h2>Olá NOMEEEEE</h2>
+            <p>Recebemos seu e-mail e retornaremos o mais rápido possivel.</p><br>
+
+            <p><small>Att,</small><br><b style='color: #11254c'>Brech&oacute; Adventure</b></p>
+            <hr style='border: 0; border-top: 1px solid #cecece;'>
+            <p style='text-align: center; color: #777;'><small>** Este &eacute; um e-mail autom&aacute;tico. Favor n&atilde;o respond&ecirc;-lo **</small></p>
+        </td>
+    </tr>
+@endsection;
---
+++ resources/views/email/template.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang='pt-BR'>
+    <head>
+        <meta charset='UTF-8'>
+    </head>
+    <body>
+        <table align='center' style='width: 600px;'>
+            <tr>
+                <td style='background-color: #F3BC55;' align='center'>
+                    <h3>BRECHÓ ADVENTURE<h3>
+                </td>
+            </tr>
+            @yield('content');
+        </table>
+    </body>
+</html>
